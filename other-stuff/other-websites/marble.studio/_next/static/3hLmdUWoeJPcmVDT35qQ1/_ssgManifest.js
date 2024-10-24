a961a28d866cca1fbf070efd0274b65cc0ee331d
Rod Hooker <rod@ago.consulting>
2024-10-23 20:59:35 -0500

    2024-10-23

--- other-stuff/other-websites/marble.studio/_next/static/3hLmdUWoeJPcmVDT35qQ1/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F404","\u002F[[...slug]]","\u002Farticle\u002F[slug]","\u002Fcommunity","\u002Fcompanies","\u002Fcompanies\u002F[slug]","\u002Fopportunities","\u002Fopportunities\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
